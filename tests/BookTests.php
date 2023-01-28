50ee097a74ee5008340824ed3c1df353bac38e85
B-Musick <bmuze1@gmail.com>
2023-01-27 19:18:37 -0600

    Set up XML and composer file so that classes are automatically loaded into test files. Added more BookTest methods.

--- tests/BookTests.php
+++ tests/BookTests.php
@@ -1,22 +1,44 @@
 <?php
 use \PHPUnit\Framework\TestCase;
+use App\Book;
 
 class BookTests extends TestCase
 {
-    public function testCorrectISBNReturned(){
-        require './src/classes/Book.php';
+    protected $book, $book2;
 
-        $book = new Book(1, "Supernova Era", "Penguin", "01/01/01");
-        $book2 = new Book(2102, "The Lost World", "Penguin", "01/02/02");
+    /**
+     * setUp()
+     * - This is run before each function
+     */
+    protected function setUp() : void{
+        $this->book = new Book(1, "Supernova Era", "Penguin", "01/01/01");
+        $this->book2 = new Book(2102, "The Lost World", "Penguin", "01/02/02");
+    }
 
-        $this->assertEquals($book->getID(), 1);
-        $this->assertEquals($book->getISBN(),1);
-        $this->assertEquals($book->getTitle(),"Supernova Era");
-        $this->assertEquals($book->getPublisher(),"Penguin");
-        $this->assertEquals($book->getDate(),"01/01/01");
-
+    public function testCorrectIDReturned() : void {
         // Want to make sure the class variable increments for each book made
-        $this->assertEquals($book2->getID(), 2);
+        $this->assertEquals($this->book->getID(), 1);
+        $this->assertEquals($this->book2->getID(), 2);
+    }
+
+    public function testCorrectISBNReturned() : void {
+        $this->assertEquals($this->book->getISBN(),1);
+        $this->assertEquals($this->book2->getISBN(),2102);
+    }
+
+    public function testCorrectTitleReturned() : void {
+        $this->assertEquals($this->book->getTitle(),"Supernova Era");
+        $this->assertEquals($this->book2->getTitle(),"The Lost World");
+    }
+
+    public function testCorrectPublisherReturned() : void{
+        $this->assertEquals($this->book->getPublisher(),"Penguin");
+        $this->assertEquals($this->book2->getPublisher(),"Penguin");
+    }
+
+    public function testCorrectDate(){
+        $this->assertEquals($this->book->getDate(),"01/01/01");
+        $this->assertEquals($this->book2->getDate(),"01/02/02");
     }
 }
 ?>
